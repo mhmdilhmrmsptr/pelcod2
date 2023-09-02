a9b63a5d4e4dbae19fb238ca58b7aa3ba6e09c1b
mhmdilhmrmsptr <143860008+mhmdilhmrmsptr@users.noreply.github.com>
2023-09-02 10:59:54 +0700

    Add files via upload
    coba upload file

---
+++ cobaapyh.py
@@ -0,0 +1,15 @@
+nama_saya = str(input("masukan nama kamu = "))
+print(nama_saya, "nama saya hffh")
+
+umur = int(input("masukan umur kamu = "))
+print(umur, "bertipe", type(umur))
+
+IPK = float(input("masukan IPK kamu = "))
+print(IPK, "bertipe", type(IPK))
+
+menikah = bool(input())
+print("menikah?", menikah, "bertipe", type(menikah))
+
+# matkul_dilab1
+print("matkul lain: ",matkul_dilab1, "bertipe", type(matkul_dilab1))
+
